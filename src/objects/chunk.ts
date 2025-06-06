30cc3b1531ea7de69062bb28fd72b390d9ee54db
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2025-06-06 14:44:32 +0200

    chore: scene colors

--- src/objects/chunk.ts
+++ src/objects/chunk.ts
@@ -3,7 +3,7 @@ import { Mesh, MeshStandardMaterial, PlaneGeometry } from "three";
 export const createChunk = () => {
   const planeGeo = new PlaneGeometry(10, 10);
   const planeMat = new MeshStandardMaterial({
-    color: 0x00ff00,
+    color: 0x47b598,
     transparent: true,
   });
   const plane = new Mesh(planeGeo, planeMat);
